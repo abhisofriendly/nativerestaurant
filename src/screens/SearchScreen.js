dcf927653b137ba5cacd819d56ee13d9c65ea649
abhisofriendly <abhisofriendly@gmail.com>
2021-03-04 13:25:25 +0530

    added screens

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen =()=>{
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
+    const [searchAPi, results, errorMessage ] = useResults();
 
-    const searchAPi = async ()=>{
-        try{
-
-            const response = await yelp.get();
-            console.log(response);
-        }catch(error){
-            console.log(error)
-        }
+    const filterResultByPrice = (price)=>{
+        return results.filter((result)=>{
+            return result.price === price;
+        })
     }
 
-
     return(
-        <View>
+        <>
             <SearchBar 
                 term={term} 
                 onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmit={()=>console.log('term submitted')} />
-            <Text>Search screen in action</Text>
-            <Text>{results.length} results</Text>
-            <Button title="hello click" onPress={()=>searchAPi()} />
-        </View>
+                onTermSubmit={()=>searchAPi(term)} />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            {/* <Text>{results.length} results</Text> */}
+
+            <ScrollView>
+                <ResultsList result={filterResultByPrice('$')} title="Cost Effective" />
+                <ResultsList result={filterResultByPrice('$$')} title="Bit Pricer" />
+                <ResultsList result={filterResultByPrice('$$$')} title="Big Spender" />
+            </ScrollView>
+        </>
     )
 }
 
